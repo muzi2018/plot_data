3bef81967c17fa86282596375d21bcf9b821f6f2
muzi_0764 <duangsir518@163.com>
2024-11-22 13:11:55 +0100

    New function
    Move axis to inside

--- Untitled-1.py
+++ Untitled-1.py
@@ -24,8 +24,15 @@ for i in range(0, N_intensity):
 # print("WaveLength:",WaveLength)
 # print("Intensity:",Intensity)
 n_lines = N_intensity
-cmap = mpl.colormaps['plasma']
-colors = cmap(np.linspace(0, 1, n_lines))
+cmap = mpl.colormaps['Blues']
+# cmap = mpl.colormaps['inferno']
+
+# cmap = mpl.colormaps['Pastel1'] 
+# cmap = mpl.colormaps['cividis'] 
+
+
+
+colors = cmap(np.linspace(0, n_lines/30, n_lines))
 fig, ax = plt.subplots()
 for i, color in enumerate(colors):
     ax.plot(WaveLength, Intensity[:, i], color=color)
@@ -45,88 +52,18 @@ cbar.set_label('Time(min)')  # Label for the color bar
 ax.set_xlabel("Wavelength (nm)")
 ax.set_ylabel("Intensity")
 
+# Move spines (XY axis lines) inside the plot
+# ax.spines['top'].set_position('zero')  # Move the top spine to inside
+# ax.spines['right'].set_position('zero')  # Move the right spine to inside
+# ax.spines['left'].set_position('zero')  # Move the left spine to inside
+# ax.spines['bottom'].set_position('zero')  # Move the bottom spine to inside
+
+# # Make sure the ticks are inside the plot too
+ax.tick_params(axis='x', direction='in', length=6)
+ax.tick_params(axis='y', direction='in', length=6)
+
+
 plt.show()
 
 exit()
 
-
-
-
-
-    
-# print("Intensity = ", Intensity)
-    # for j in range()
-    # column_array = df.iloc[i, 0].to_numpy() 
-    # print("column_array = ", column_array)
-    # Intensity[i, :, 0] = column_array.flatten()
-
-
-# print("Wave Length:", WaveLength)
-# print("--------------------")
-
-### (wave_length[0, :], intensity[0, 0, :]) - (wave_length[0, :], intensity[0, 1, :]) -...-
-### (wave_length[0, :], intensity[0, 856, :])
-
-
-
-
-### Plotting parameter###
-n_lines = N
-cmap = mpl.colormaps['plasma']
-colors = cmap(np.linspace(0, 1, n_lines))
-fig, ax = plt.subplots()
-
-
-
-# ### Plotting data ###
-# arr1 = np.array([1, 2, 3, 4, 5])
-# # From a tuple
-# arr2 = np.array((1, 3, 1, 2, 1))
-# ax.plot(arr1, arr2)
-
-
-
-
-for i, color in enumerate(colors):
-    ax.plot([arr1, arr2], color=color)
-
-# print("Row Array:", row_array)  # Output: [1 2 3 4]
-# print("Column Array:", col_array)  # Output: [5 6 7 8]
-
-
-
-
-
-n_lines = 21
-cmap = mpl.colormaps['plasma']
-
-# Take colors at regular intervals spanning the colormap.
-colors = cmap(np.linspace(0, 1, n_lines))
-
-fig, ax = plt.subplots()
-
-arr1 = np.array([1, 2, 3, 4, 5])
-# From a tuple
-arr2 = np.array((1, 3, 1, 2, 1))
-ax.plot(arr1, arr2)
-
-# Plot the lines
-
-## 
-
-# for i, color in enumerate(colors):
-#     ax.plot([arr1, arr2], color=color)
-
-
-
-
-# Create a ScalarMappable for the color bar
-norm = mpl.colors.Normalize(vmin=0, vmax=n_lines - 1)  # Normalize line indices to colormap
-sm = mpl.cm.ScalarMappable(cmap=cmap, norm=norm)
-sm.set_array([])  # Required for the color bar
-
-# Add the color bar to the figure
-cbar = fig.colorbar(sm, ax=ax)
-cbar.set_label('Line Index')  # Label for the color bar
-
-plt.show()
